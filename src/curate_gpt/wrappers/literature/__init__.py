f163c0bb867e3ca7b4618b8bc0065b885db4c291
Mayank-Pratap07 <Mayankpratap.singh@studenti.unimi.it>
2024-07-07 22:14:03 +0200

    Delete src/curate_gpt/wrappers directory

--- src/curate_gpt/wrappers/literature/__init__.py
+++
@@ -1,9 +0,0 @@
-from .bioc_wrapper import BiocWrapper
-from .pubmed_wrapper import PubmedWrapper
-from .wikipedia_wrapper import WikipediaWrapper
-
-__all__ = [
-    "BiocWrapper",
-    "PubmedWrapper",
-    "WikipediaWrapper",
-]
